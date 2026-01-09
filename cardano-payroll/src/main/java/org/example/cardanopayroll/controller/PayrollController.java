733edb0bf0f0ac7f945fd522b5d7781cef0c5ad3
Frozen-369 <frozenboyarp@gmail.com>
2026-01-09 16:43:33 +0545

    Final Deployment

--- cardano-payroll/src/main/java/org/example/cardanopayroll/controller/PayrollController.java
+++ cardano-payroll/src/main/java/org/example/cardanopayroll/controller/PayrollController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class PayrollController {
 
     private final PayrollService payrollService;
--- cardano-payroll/src/main/java/org/example/cardanopayroll/controller/WalletController.java
+++ cardano-payroll/src/main/java/org/example/cardanopayroll/controller/WalletController.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class WalletController {
 
     private final WalletService walletService;
