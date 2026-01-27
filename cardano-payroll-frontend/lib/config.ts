f28cfad8dd7b46f2091eecbc629f1e2271ff8f3e
Arpit-K-Sharma <a.k.sharma0369@gmail.com>
2026-01-27 10:45:58 +0545

    minor bug fixes

--- cardano-payroll-frontend/lib/config.ts
+++ cardano-payroll-frontend/lib/config.ts
@@ -1,8 +1,10 @@
+
 const config = {
   kuberApiUrl: process.env.NEXT_PUBLIC_KUBER_API_URL as string,
   kuberApiKey: process.env.NEXT_PUBLIC_KUBER_API_KEY as string,
   apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL as string,
   companyWalletAddress: process.env.NEXT_PUBLIC_COMPANY_WALLET_ADDRESS as string,
+  cardanoNetwork: process.env.NEXT_PUBLIC_CARDANO_NETWORK as 'mainnet' | 'preprod' | 'preview',
 };
 
 export default config;
